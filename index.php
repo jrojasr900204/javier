430876a44181703106c1b0ab115994951f7786c0
jrojasr900204 <jrojasr9002040@gmail.com>
2015-08-27 13:53:31 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ echo('<center>');
 echo('<b>');
 echo('<i>');
 echo('Soy  yo  mi  vida, estoy  publicando el  sitio  de nosotros en  la nueve');
-echo('te quiero mucho mucho.);
+echo('te quiero mucho mucho');
 echo('No cojas lucha con  el  trabajo que ya aparecera algo');
 echo('besos grandes');
 echo('</i>');
