fcf318f78ef3d61c349ccbb3d38522a9d3424580
jrojasr900204 <jrojasr9002040@gmail.com>
2015-08-27 11:08:04 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+echo('<center>');
+echo('<b>');
+echo('<i>');
+echo('Javier Rojas de la Rosa');
+echo('</i>');
+echo('</b>');
+echo('</center>');
+echo('</br>');
+echo('</br>');
+echo phpinfo();
+?>
